bbc0b3ef0cadcd2d9676c718051c71899f4e217e
okkyputra <okky.putra4@gmail.com>
2023-07-20 16:42:11 +0700

    commit

--- DataPreprocessing/DataPreprocessingtools.py
+++ DataPreprocessing/DataPreprocessingtools.py
@@ -4,33 +4,31 @@
 import numpy as np
 import matplotlib.pyplot as pyl
 import pandas as pd
-
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 
 #Importing needed dataset
 dataset = pd.read_csv('Data.csv')
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
-
 #print(x)
 #print(y)
 
-
 #Handle missing data on dataset
-from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 imputer.fit(x[:, 1:3])
 x[:, 1:3] = imputer.transform(x[:, 1:3])
 print(x)
 
 #Encoding categorical data
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
 x = np.array(ct.fit_transform(x))
 print(x)
 
 #Encoding Dependant Variable
-from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
 y = le.fit_transform(y)
 print(y)
+
+#Spliting dataset into traning set and test set
