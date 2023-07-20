800137b5b97fa8892bf14d45bdc42a2e89b530d5
okkyputra <okky.putra4@gmail.com>
2023-07-20 14:21:21 +0700

    message

---
+++ DataPreprocessing/DataPreprocessingtools.py
@@ -0,0 +1,32 @@
+#This is Data Preprocessing for Machine Learning using Python
+
+#Importing needed library
+import numpy as np
+import matplotlib.pyplot as pyl
+import pandas as pd
+
+
+#Importing needed dataset
+dataset = pd.read_csv('Data.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+#print(x)
+#print(y)
+
+
+#Handle missing data on dataset
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
+print(x)
+
+#Encoding categorical data
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+x = np.array(ct.fit_transform(x))
+
+
+print(x)
