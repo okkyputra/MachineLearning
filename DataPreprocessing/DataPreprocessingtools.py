e43688f2cc12af40007889aff0d98c24ac583cdb
okkyputra <okky.putra4@gmail.com>
2023-07-27 14:43:21 +0700

    commit

--- DataPreprocessing/DataPreprocessingtools.py
+++ DataPreprocessing/DataPreprocessingtools.py
@@ -7,6 +7,7 @@ import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.model_selection import train_test_split
 
 #Importing needed dataset
 dataset = pd.read_csv('Data.csv')
@@ -19,16 +20,24 @@ y = dataset.iloc[:, -1].values
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 imputer.fit(x[:, 1:3])
 x[:, 1:3] = imputer.transform(x[:, 1:3])
-print(x)
+#print(x)
 
 #Encoding categorical data
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
 x = np.array(ct.fit_transform(x))
-print(x)
+#print(x)
 
 #Encoding Dependant Variable
 le = LabelEncoder()
 y = le.fit_transform(y)
-print(y)
+#print(y)
 
 #Spliting dataset into traning set and test set
+X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size = 0.2, random_state= 1)
+print('xtrain : ', X_train)
+print('xtest : ', X_test)
+print('ytrain : ', Y_train)
+print('ytest ; ', Y_test)
+
+#feature scaling
+
