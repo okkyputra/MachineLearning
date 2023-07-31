7c322c59a941840c0de4da995eb870b0b775e2dd
okkyputra <okky.putra4@gmail.com>
2023-07-31 10:39:17 +0700

    commit

--- DataPreprocessing/DataPreprocessingtools.py
+++ DataPreprocessing/DataPreprocessingtools.py
@@ -6,8 +6,9 @@ import matplotlib.pyplot as pyl
 import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
+
 
 #Importing needed dataset
 dataset = pd.read_csv('Data.csv')
@@ -34,10 +35,16 @@ y = le.fit_transform(y)
 
 #Spliting dataset into traning set and test set
 X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size = 0.2, random_state= 1)
-print('xtrain : ', X_train)
-print('xtest : ', X_test)
-print('ytrain : ', Y_train)
-print('ytest ; ', Y_test)
+#print('xtrain : ', X_train)
+#print('xtest : ', X_test)
+#print('ytrain : ', Y_train)
+#print('ytest ; ', Y_test)
 
 #feature scaling
+sc = StandardScaler()
+X_train[:, 3:] = sc.fit_transform(X_train[:, 3:])
+X_test[:, 3:] = sc.fit_transform(X_test[:, 3:])
 
+#result feature scaling
+print('xtrain : ', X_train)
+print('xtest : ', X_test)
